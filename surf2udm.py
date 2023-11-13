4d902e43e1e126aa1f933f3552c0532aacf3856c
Alex Mueller <alex.mueller@roche.com>
2023-11-13 14:37:10 +0100

    :recycle: UDM script

--- surf2udm.py
+++ surf2udm.py
@@ -16,7 +16,10 @@ from typing_extensions import Annotated
 
 from surf_utils.mappings import doi_pattern
 
-logging.basicConfig(format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(
+    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -33,9 +36,15 @@ def remove_none_attrib(elem):
 
 @app.command()
 def surf2ord(
-    input_file: Annotated[str, typer.Argument(help="name of the input file in SURF format")],
-    output_file: Annotated[str, typer.Argument(help="name of the output file in XML format; suffix: .xml")],
-    delimiter: Annotated[str, typer.Argument(help="delimiter of the input file")] = "\t",
+    input_file: Annotated[
+        str, typer.Argument(help="name of the input file in SURF format")
+    ],
+    output_file: Annotated[
+        str, typer.Argument(help="name of the output file in XML format; suffix: .xml")
+    ],
+    delimiter: Annotated[
+        str, typer.Argument(help="delimiter of the input file")
+    ] = "\t",
 ):
     # read the SURF file
     df = pd.read_csv(input_file, delimiter=delimiter)
@@ -47,16 +56,25 @@ def surf2ord(
 
     # check if yields are 0-1 or 0-100
     if df["product_1_yield"].max() <= 3.0:
-        logger.warning("Yields seem to be provided as fractions instead of percentages. Multiplying by 100")
+        logger.warning(
+            "Yields seem to be provided as fractions instead of percentages. Multiplying by 100"
+        )
         for c in df.columns:
             if c.endswith("yield"):
                 df[c] = df[c] * 100.0
 
     # define UDM headers and main structure
-    udm = ET.Element("UDM", DATABASE="SURF", SEQUENCE="001", TIMESTAMP=datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
+    udm = ET.Element(
+        "UDM",
+        DATABASE="SURF",
+        SEQUENCE="001",
+        TIMESTAMP=datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+    )
     udm.set("xmlns:xsi", '"http://www.w3.org/2001/XMLSchema-instance"')
     udm.set("xmlns:xsd", "http://www.w3.org/2001/XMLSchema")
-    version = ET.SubElement(udm, "UDM_VERSION", MAJOR="6", MINOR="0", REVISION="0", VERSIONTEXT="6.0.0")
+    version = ET.SubElement(
+        udm, "UDM_VERSION", MAJOR="6", MINOR="0", REVISION="0", VERSIONTEXT="6.0.0"
+    )
     legal = ET.SubElement(udm, "LEGAL")
     citations = ET.SubElement(udm, "CITATIONS")
     molecules = ET.SubElement(udm, "MOLECULES")
@@ -67,7 +85,9 @@ def surf2ord(
     cnt_cit = 1
 
     invalid = []
-    for idx, row in track(df.iterrows(), total=len(df), description="Reading molecules..."):
+    for idx, row in track(
+        df.iterrows(), total=len(df), description="Reading molecules..."
+    ):
         row = row.dropna()
         err = False
 
@@ -87,40 +107,58 @@ def surf2ord(
                     "MOLSTRUCTURE": MolToMolBlock(mol)
                 }  # store the compound using the inchikey as index
                 if cas:
-                    cas = re.sub(r"-0([0-9])$", r"-\g<1>", cas)  # replace potential leading zero for last CAS pos
+                    cas = re.sub(
+                        r"-0([0-9])$", r"-\g<1>", cas
+                    )  # replace potential leading zero for last CAS pos
                     mol_dict[MolToInchiKey(mol)].update({"CAS": cas})
             else:
-                logger.error(f"Can't process SMILES/InChI of {cpd} in reaction {row.rxn_id}")
+                logger.error(
+                    f"Can't process SMILES/InChI of {cpd} in reaction {row.rxn_id}"
+                )
                 err = True
                 continue
 
         # reading the provenance / citations
-        if "source_id" in row and re.match(doi_pattern, row.source_id):  # catch DOI if present
+        if "source_id" in row and re.match(
+            doi_pattern, row.source_id
+        ):  # catch DOI if present
             doi = re.findall(doi_pattern, row.source_id)[0]
             if doi not in cit_dict:
                 cit_dict[doi] = cnt_cit
                 cit = ET.SubElement(citations, "CITATION", ID=str(cnt_cit))
                 ET.SubElement(cit, "DOI").text = doi
+                # if "source_type" in row:
+                #     ET.SubElement(cit, "TYPE").text = row["source_type"]
                 cnt_cit += 1
 
         # exclude reaction if error observed in one of the molecules of this reaction
         if err:
             invalid.append(row.rxn_id)
-            logger.error(f"Couln't process all SMILES in reaction {row.rxn_id}, skipping reaction")
+            logger.error(
+                f"Couln't process all SMILES in reaction {row.rxn_id}, skipping reaction"
+            )
 
     # processing the individual, unique compounds
-    for k, v in track(mol_dict.items(), total=len(mol_dict), description="Adding molecules to UDM..."):
+    for k, v in track(
+        mol_dict.items(), total=len(mol_dict), description="Adding molecules to UDM..."
+    ):
         mol = ET.SubElement(molecules, "MOLECULE", ID=k)
-        ET.SubElement(mol, "MOLSTRUCTURE").text = "<![CDATA[" + v["MOLSTRUCTURE"] + "]]>"
+        ET.SubElement(mol, "MOLSTRUCTURE").text = (
+            "<![CDATA[" + v["MOLSTRUCTURE"] + "]]>"
+        )
         if "CAS" in v:
             ET.SubElement(mol, "CAS").text = v["CAS"]
 
     if invalid:
-        logger.warning(f"Ignoring {len(invalid)} reactions where SMILES could not be read for one of the components")
+        logger.warning(
+            f"Ignoring {len(invalid)} reactions where SMILES could not be read for one of the components"
+        )
         df = df[~df.rxn_id.isin(invalid)]  # exclude invalid reactions
 
     # processing the individual reactions
-    for idx, row in track(df.iterrows(), total=len(df), description="Processing reactions..."):
+    for idx, row in track(
+        df.iterrows(), total=len(df), description="Processing reactions..."
+    ):
         if "rxn_id" not in row:
             logger.error(f"Reaction ID missing! Can't process reaction {idx}")
             continue
@@ -130,7 +168,9 @@ def surf2ord(
         reaction = ET.SubElement(reactions, "REACTION", ID=row.rxn_id.replace("_", "-"))
 
         # add a variation, sections and conditions
-        if "source_id" in row and re.match(doi_pattern, row.source_id):  # catch DOI if present
+        if "source_id" in row and re.match(
+            doi_pattern, row.source_id
+        ):  # catch DOI if present
             doi = re.findall(doi_pattern, row.source_id)[0]
             variation = ET.SubElement(reaction, "VARIATION", CIT_ID=str(cit_dict[doi]))
         else:
@@ -147,6 +187,9 @@ def surf2ord(
         if "time_h" in row:
             tme = ET.SubElement(conditions, "TIME")  # default unit is hour
             ET.SubElement(tme, "exact").text = f"{row.time_h:.1f}"
+        if "atmosphere" in row:
+            atmo = ET.SubElement(conditions, "ATMOSPHERE")
+            atmo.text = row.atmosphere
 
         for cpd in sorted(set(re.findall("\w+_[1-9]", " ".join(row.index.tolist())))):
             if (
@@ -155,13 +198,19 @@ def surf2ord(
                 and f"{cpd}_cas" not in row
                 and f"{cpd}_name" not in row
             ):
-                logger.error(f"no SMILES, InChI, CAS or Name forund for {cpd} in {row.rxn_id}, skipping reaction")
+                logger.error(
+                    f"no SMILES, InChI, CAS or Name forund for {cpd} in {row.rxn_id}, skipping reaction"
+                )
                 err = True
                 continue
 
             # get the compound name
             cpd_name = row.get(
-                f"{cpd}_cas", row.get(f"{cpd}_name", row.get(f"{cpd}_smiles", row.get(f"{cpd}_inchi", "unknown")))
+                f"{cpd}_cas",
+                row.get(
+                    f"{cpd}_name",
+                    row.get(f"{cpd}_smiles", row.get(f"{cpd}_inchi", "unknown")),
+                ),
             )
 
             # create an InChI Key as the identifier to link molecules to reactions
@@ -170,7 +219,9 @@ def surf2ord(
             elif f"{cpd}_inchi" in row:
                 inchikey = MolToInchiKey(MolFromInchi(row[f"{cpd}_inchi"]))
             else:
-                logger.error(f"no SMILES/InChI forund for {cpd} in {row.rxn_id}, skipping reaction")
+                logger.error(
+                    f"no SMILES/InChI forund for {cpd} in {row.rxn_id}, skipping reaction"
+                )
                 err = True
                 continue
 
@@ -190,7 +241,9 @@ def surf2ord(
                 # check if equivalents and a reaction scale are provided
                 if f"{cpd}_eq" in row and "scale_mol" in row and row.scale_mol > 0:
                     rct_amnt = ET.SubElement(reactant, "AMOUNT")
-                    rct_amnt.text = f"{row.scale_mol * row[f'{cpd}_eq'] * 1000:.4f} mmol"  # mmol
+                    rct_amnt.text = (
+                        f"{row.scale_mol * row[f'{cpd}_eq'] * 1000:.4f} mmol"  # mmol
+                    )
             else:
                 if "solvent" in cpd:
                     solvent = ET.SubElement(section, "SOLVENT")
@@ -206,12 +259,16 @@ def surf2ord(
                             scale = float(row.scale_mol)
                             conc = float(row.concentration_mol_l)
                             solv_amnt = ET.SubElement(solvent, "AMOUNT")
-                            if f"{cpd}_fraction" in row:  # check if fraction available (multiple solvents present)
+                            if (
+                                f"{cpd}_fraction" in row
+                            ):  # check if fraction available (multiple solvents present)
                                 solv_amnt.text = f'{row[f"{cpd}_fraction"] * scale / conc * 1000:.4f} ml'  # ml
                             else:
                                 solv_amnt.text = f"{scale / conc * 1000:.4f} ml"  # ml
                         except ValueError:
-                            logger.warning(f"Can't process {cpd}_fraction to amount in reaction {row.rxn_id}")
+                            logger.warning(
+                                f"Can't process {cpd}_fraction to amount in reaction {row.rxn_id}"
+                            )
                 elif "catalyst" in cpd:
                     cat = ET.SubElement(section, "CATALYST")
                     ET.SubElement(cat, "MOLECULE", MOL_ID=inchikey).text = ""
@@ -230,7 +287,13 @@ def surf2ord(
     remove_none_attrib(udm)
     ET.indent(udm)
     with open(output_file, "w") as f:
-        out = ET.tostring(udm, method="xml", encoding="utf-8", xml_declaration=True, short_empty_elements=True)
+        out = ET.tostring(
+            udm,
+            method="xml",
+            encoding="utf-8",
+            xml_declaration=True,
+            short_empty_elements=True,
+        )
         # write the UDM to XML, replacing the escaped CDATA characters again
         f.write(
             out.decode("utf-8")
--- udm2surf.py
+++ udm2surf.py
@@ -11,13 +11,17 @@ import xmltodict
 from bs4 import BeautifulSoup
 from lxml import etree
 from rdkit.Chem import MolFromMolBlock, MolToSmiles
+from rich.progress import track
 from typing_extensions import Annotated
 
 app = typer.Typer()
 
 # TODO: get complete UDM example and finalize
 
-logging.basicConfig(format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(
+    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -34,21 +38,32 @@ def validate_udm_file(xml_file):
 
 @app.command()
 def udm2smiles(
-    input_file: Annotated[str, typer.Argument(help="name of the input file in UDM format")],
-    output_file: Annotated[str, typer.Argument(help="name of the output file in ORD format; suffixes: .pbtxt or .pb")],
-    delimiter: Annotated[str, typer.Argument(help="delimiter of the input file")] = "\t",
+    input_file: Annotated[
+        str, typer.Argument(help="name of the input file in UDM format")
+    ],
+    output_file: Annotated[
+        str,
+        typer.Argument(
+            help="name of the output file in SURF format; .tsv or .txt format"
+        ),
+    ],
     validate: Annotated[
-        bool, typer.Option(help="Whether to overwrite existing provenance with provided info.")
+        bool,
+        typer.Option(
+            help="Whether to validate the UDM file structure versus the UDM schema."
+        ),
     ] = True,
 ):
     if validate:  # validate the XML file versus the UDM schema
         errors = validate_udm_file(input_file)
         if errors:
-            logger.error(f"UDM schema validation found the following {len(errors)} errors:")
+            logger.error(
+                f"UDM schema validation found the following {len(errors)} errors:"
+            )
             for e in errors:
                 logger.error(e)
         else:
-            logger.info("Validation successful")
+            logger.info("UDM validation successful")
 
     # read UDM data
     with open(input_file, "r") as f:
@@ -65,12 +80,20 @@ def udm2smiles(
     legal = xml.find_all("LEGAL")
 
     # read all citations
-    citations = xml.find("CITATIONS")
+    citations = {}
+    for cit in xml.find("CITATIONS").find_all("CITATION"):
+        citations[cit.attrs["ID"]] = {}
+        if cit.find("DOI"):
+            citations[cit.attrs["ID"]].update({"DOI": cit.DOI.text})
+        if cit.find("TYPE"):
+            citations[cit.attrs["ID"]].update({"TYPE": cit.TYPE.text})
 
     # read all molecules
     molecules = xml.find("MOLECULES")
     mol_dict = {}
-    for mol in molecules.find_all("MOLECULE"):
+    for mol in track(
+        molecules.find_all("MOLECULE"), description="Reading molecule structures..."
+    ):
         m = MolFromMolBlock(mol.MOLSTRUCTURE.text)
         d = xmltodict.parse(mol.prettify())["MOLECULE"]
         if m:
@@ -84,11 +107,23 @@ def udm2smiles(
 
     # read all reactions
     reactions = xml.find("REACTIONS")
-    for reaction in reactions.find_all("REACTION"):
+    for reaction in track(
+        reactions.find_all("REACTION"), description="Reading reactions..."
+    ):
         row = pd.Series()
         rxn_id = reaction.attrs["ID"]
         row["rxn_id"] = rxn_id
-        # Reactant
+        # Products
+        try:
+            prod_ids = [p.text for p in reaction.find_all("PRODUCT_ID")]
+            products = {i: mol_dict[i] for i in prod_ids}
+        except KeyError:
+            logger.error(
+                f"One of these product IDs {prod_ids} was not found in molecules! Skipping reaction {rxn_id}"
+            )
+            continue
+
+        # Reactants -> Startingmaterials
         try:
             react_ids = [r.text for r in reaction.find_all("REACTANT_ID")]
             startmats = {i: mol_dict[i] for i in react_ids}
@@ -97,54 +132,138 @@ def udm2smiles(
                 f"One of these reactant IDs {react_ids} was not found in molecules! Skipping reaction {rxn_id}"
             )
             continue
-        # Product
-        try:
-            prod_ids = [p.text for p in reaction.find_all("PRODUCT_ID")]
-            products = {i: mol_dict[i] for i in prod_ids}
-        except KeyError:
-            logger.error(f"One of these product IDs {prod_ids} was not found in molecules! Skipping reaction {rxn_id}")
-            continue
-        # Variations
+
+        # Variations (first update all reactant amounts to get the scale)
         for variation in reaction.find_all("VARIATION"):
-            yields = dict()
-            for p in variation.find_all("PRODUCT"):
-                yields[p.find("MOLECULE").attrs["MOL_ID"]] = float(p.find("YIELD").text)
-            reagents = dict()
-            for reagent in variation.find_all("REAGENT"):
-                reag_id = reagent.MOLECULE.attrs["MOL_ID"]
-                try:
-                    reagents[reag_id] = mol_dict[reag_id]
-                    reagents[reag_id]["AMOUNT"] = reagent.find("AMOUNT").text
-                except KeyError:
-                    logger.error(f"Reagent ID {reag_id} not found in molecules! Skipping this reagent in {rxn_id}")
-            catalysts = dict()
-            for cat in variation.find_all("CATALYST"):
-                cat_id = cat.MOLECULE.attrs["MOL_ID"]
-                try:
-                    catalysts[cat_id] = mol_dict[cat_id]
-                    catalysts[cat_id]["AMOUNT"] = cat.find("AMOUNT").text
-                except KeyError:
-                    logger.error(f"Catalyst ID {cat_id} not found in molecules! Skipping this catalyst in {rxn_id}")
-            reactants = dict()
+            # update reactants and scale
             for react in variation.find_all("REACTANT"):
                 react_id = react.MOLECULE.attrs["MOL_ID"]
                 try:
-                    reactants[react_id] = mol_dict[react_id]
-                    reactants[react_id]["AMOUNT"] = react.find("AMOUNT").text
+                    startmats[react_id] = mol_dict[react_id]
+                    startmats[react_id]["AMOUNT"] = react.find("AMOUNT").text
                 except KeyError:
-                    logger.error(f"Reactant ID {react_id} not found in molecules! Skipping this reagent in {rxn_id}")
-            conditions = variation.find("CONDITIONS")
+                    logger.error(
+                        f"Reactant ID {react_id} not found in molecules! Skipping this reagent in {rxn_id}"
+                    )
+        row["scale_mol"] = (
+            min(
+                [
+                    float(v["AMOUNT"].replace("mmol", "").strip())
+                    for v in startmats.values()
+                ]
+            )
+            * 0.001
+        )
+        # Variations continued
+        for variation in reaction.find_all("VARIATION"):
+            if "CIT_ID" in variation.attrs:
+                if "DOI" in citations[variation.attrs["CIT_ID"]]:
+                    row["source_id"] = citations[variation.attrs["CIT_ID"]]["DOI"]
+                if "TYPE" in citations[variation.attrs["CIT_ID"]]:
+                    row["source_type"] = citations[variation.attrs["CIT_ID"]]["TYPE"]
+            # update product yields
+            for p in variation.find_all("PRODUCT"):
+                products[p.find("MOLECULE").attrs["MOL_ID"]].update(
+                    {"YIELD": float(p.find("YIELD").text)}
+                )
+            # parse reagents
+            for i, reag in enumerate(variation.find_all("REAGENT")):
+                reag_id = reag.MOLECULE.attrs["MOL_ID"]
+                try:
+                    row[f"reagent_{i+1}_name"] = reag_id
+                    row[f"reagent_{i+1}_smiles"] = mol_dict[reag_id]["MOLSTRUCTURE"]
+                    if "CAS" in mol_dict[reag_id]:
+                        row[f"reagent_{i+1}_cas"] = mol_dict[reag_id]["CAS"]
+                    row[f"reagent_{i+1}_eq"] = float(
+                        reag.AMOUNT.text.replace("mmol", "").strip()
+                    ) / (row["scale_mol"] * 1000)
+                except KeyError:
+                    logger.error(
+                        f"Reagent ID {reag_id} not found in molecules! Skipping this reagent in {rxn_id}"
+                    )
+            # parse catalysts
+            for i, cat in enumerate(variation.find_all("CATALYST")):
+                cat_id = cat.MOLECULE.attrs["MOL_ID"]
+                try:
+                    row[f"catalyst_{i+1}_name"] = cat_id
+                    row[f"catalyst_{i+1}_smiles"] = mol_dict[cat_id]["MOLSTRUCTURE"]
+                    if "CAS" in mol_dict[cat_id]:
+                        row[f"catalyst_{i+1}_cas"] = mol_dict[cat_id]["CAS"]
+                    row[f"catalyst_{i+1}_eq"] = float(
+                        cat.AMOUNT.text.replace("mmol", "").strip()
+                    ) / (row["scale_mol"] * 1000)
+                except KeyError:
+                    logger.error(
+                        f"Catalyst ID {cat_id} not found in molecules! Skipping this catalyst in {rxn_id}"
+                    )
+            # build solvents
+            solvents = {
+                r.MOLECULE.attrs["MOL_ID"]: mol_dict[r.MOLECULE.attrs["MOL_ID"]]
+                for r in variation.find_all("SOLVENT")
+            }
+            for r in variation.find_all("SOLVENT"):
+                solvents[r.MOLECULE.attrs["MOL_ID"]].update({"AMOUNT": r.AMOUNT.text})
+            # parse conditions
+            conditions = variation.find_all("CONDITIONS")
             if conditions:
-                if conditions.find("PREPARATION"):
-                    row["procedure"] = conditions.find("PREPARATION").text
-                if conditions.find("TEMPERATURE"):
-                    row["temperature_deg_c"] = float(conditions.find("TEMPERATURE").text.strip())
-                if conditions.find("TIME"):
-                    row["time_h"] = float(conditions.find("TIME").text.strip())
+                for cond in conditions:
+                    if cond.find("PREPARATION"):
+                        row["procedure"] = cond.find("PREPARATION").text
+                    if cond.find("TEMPERATURE"):
+                        row["temperature_deg_c"] = float(
+                            cond.find("TEMPERATURE").text.strip()
+                        )
+                    if cond.find("TIME"):
+                        row["time_h"] = float(cond.find("TIME").text.strip())
+                    if cond.find("ATMOSPHERE"):
+                        row["atmosphere"] = cond.find("ATMOSPHERE").text
 
-    # list(xsd.find("xs:element", {"name": "VARIATION"}).find("xs:complexType").find("xs:sequence").children)
+        # write products
+        for i, (k, v) in enumerate(products.items()):
+            row[f"product_{i+1}_name"] = k
+            row[f"product_{i+1}_smiles"] = v["MOLSTRUCTURE"]
+            row[f"product_{i+1}_yield"] = v["YIELD"]
+            # TODO: check how to get "TYPE" working with UDM
+            if "CAS" in v:
+                row[f"product_{i+1}_cas"] = v["CAS"]
+
+        # write starting materials
+        # get reaction scale and transform mmol to mol
+        row["scale_mol"] = (
+            max(
+                [
+                    float(v["AMOUNT"].replace("mmol", "").strip())
+                    for v in startmats.values()
+                ]
+            )
+            * 0.001
+        )
+        for i, (k, v) in enumerate(startmats.items()):
+            row[f"startingmat_{i+1}_name"] = k
+            row[f"startingmat_{i+1}_smiles"] = v["MOLSTRUCTURE"]
+            row[f"startingmat_{i+1}_eq"] = float(
+                v["AMOUNT"].replace("mmol", "").strip()
+            ) / (row["scale_mol"] * 1000)
+            if "CAS" in v:
+                row[f"startingmat_{i+1}_cas"] = v["CAS"]
+
+        # write solvents
+        v_tot = sum(
+            [float(v["AMOUNT"].replace("ml", "").strip()) for v in solvents.values()]
+        )
+        row["concentration_mol_l"] = v_tot * 1000.0 * row["scale_mol"]
+        for i, (k, v) in enumerate(solvents.items()):
+            row[f"solvent_{i+1}_name"] = k
+            row[f"solvent_{i+1}_smiles"] = v["MOLSTRUCTURE"]
+            if "CAS" in v:
+                row[f"solvent_{i+1}_cas"] = v["CAS"]
+            row[f"solvent_{i+1}_fraction"] = (
+                float(v["AMOUNT"].replace("ml", "").strip()) / v_tot
+            )
+
+        # add reaction to SURF format
+        surf = pd.concat((surf, pd.DataFrame(row).T))
 
 
 if __name__ == "__main__":
-    # raise NotImplementedError("Not yet implemented, coming soon!")
     app()
